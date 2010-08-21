eada008adb7ee8cd68bc15933078e2a8130a31e0
drewbaumann <drew@ming.ly>
2010-08-20 21:06:38 -0700

    removed migrate file in hopes of it actually working now

--- db/migrate/20100819015334_remove_when_from_visits.rb
+++
@@ -1,9 +0,0 @@
-class RemoveWhenFromVisits < ActiveRecord::Migration
-  def self.up
-    remove_column :visits, :when, :datetime
-  end
-
-  def self.down
-    add_column :visits, :when, :datetime
-  end
-end
