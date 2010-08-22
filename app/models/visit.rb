d8ab1e823a5473b76b69b8f16401b1ae52a72432
drewbaumann <drew@ming.ly>
2010-08-21 18:17:23 -0700

    added validation to the visits page

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -1,6 +1,6 @@
 class Visit < ActiveRecord::Base
   belongs_to :member
-  
+  validates_numericality_of :coloft_id
   start_date = Date.parse "#{Date.today.year}-#{Date.today.month}-01"
   end_date = Date.civil(Date.today.year, Date.today.month, -1)
   named_scope :this_month, lambda{{:conditions =>['"visits".created_at BETWEEN ? AND ?',start_date,end_date]}}
